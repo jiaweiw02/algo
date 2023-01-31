b081fe41fa7902e239acb9f748da79a0b0c26fad
jiaweiw02 <jiaweiw02@gmail.com>
2023-01-31 00:19:14 -0500

    lab3

--- labs/lab3/prog2.py
+++ labs/lab3/prog2.py
@@ -4,7 +4,7 @@ from prog1 import modexp
 # program 2
 def primality2(N):
     s = set()
-    for i in range(1000):
+    for i in range(5):
         num = random.randint(1, N-1)
         # while num in s:
         #     num = random.randint(1, N-1)
@@ -22,9 +22,11 @@ def primality2(N):
 
 if __name__ == "__main__":
     carmichael = [561, 1105, 1729, 2465, 2821, 6601, 8911, 10585, 15841,
-29341, 41041, 46657, 52633, 62745, 63973, 75361, 101101, 115921, 126217, 162401, 172081,
-188461, 252601, 278545, 294409, 314821, 334153, 340561, 399001, 410041, 449065, 488881]
-    # carmichael = [561, 1105, 1729, 2465, 2821, 6601, 8911, 10585, 15841, 29341, 41041, 46657]
+                    29341, 41041, 46657, 52633, 62745, 63973, 75361, 101101, 115921, 126217, 162401, 172081,
+                    188461, 252601, 278545, 294409, 314821, 334153, 340561, 399001, 410041, 449065, 488881]
     for a in carmichael:
-        print("{}: {}".format(a, primality2(a)))
+        if (primality2(a)):
+            print("{}\tis prime".format(a))
+        else:
+            print("{}\tis not prime".format(a))
     
