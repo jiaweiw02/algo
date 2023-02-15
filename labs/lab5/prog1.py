e3a790406d86353074dfba39812ec7172479523a
Ardennt <wuj22@rpi.edu>
2023-02-15 00:50:25 -0500

    lab5 completed

---
+++ labs/lab5/prog1.py
@@ -0,0 +1,20 @@
+import numpy
+
+
+def randQuicksort(S):
+    v = S[numpy.random.randint(0, len(S))]
+    sL = [val for val in S if val < v]
+    sV = [val for val in S if val == v]
+    sR = [val for val in S if val > v]
+    if len(sL) == 0 or len(sR) == 0:
+        return S
+    return randQuicksort(sL) + sV + randQuicksort(sR)
+
+
+if __name__ == "__main__":
+    n = int(input("n: "))  # s
+    numArr = []
+    for i in range(n):
+        numArr.append(numpy.random.randint(0, n))
+
+    print(randQuicksort(numArr))
