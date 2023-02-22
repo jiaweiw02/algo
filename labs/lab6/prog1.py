3f77a38d59391b17d75b43bd6caf41e86b1aad17
jiaweiw02 <jiaweiw02@gmail.com>
2023-02-22 11:59:24 -0500

    hw5 update

--- labs/lab6/prog1.py
+++ labs/lab6/prog1.py
@@ -21,12 +21,25 @@ def reverseGraph(graph):
     return G_r
 
 
-def mark(G, v, visited, order):
+def createOrdering(orderings, max_post):
+    # create an array of size max_post
+    arr = [0 for i in range(max_post)]
+
+    for node in orderings:
+        index = orderings[node][1] - 1
+        arr[index] = node
+
+    return arr
+
+
+def mark(G, v, visited, order, max_post):
     # dont go in visited nodes
     if v in visited:
         return order
     # mark visited with preorder
     visited[v] = [order, None]
+    if order > max_post[0]:
+        max_post[0] = order
     order += 1
 
     if v in G:
@@ -37,30 +50,57 @@ def mark(G, v, visited, order):
             if neighbor not in visited:
                 backtrack = False
                 # track the order
-                order = mark(G, neighbor, visited, order)
+                order = mark(G, neighbor, visited, order, max_post)
         if backtrack:
             # if all neighbors have been visited, mark the node with its postorder
             visited[v][1] = order
+            if order > max_post[0]:
+                max_post[0] = order
             order += 1
     # nodes with no neighbors
     # mark it with its postorder
     if visited[v][1] is None:
         visited[v][1] = order
+        if order > max_post[0]:
+            max_post[0] = order
         order += 1
     return order
 
 
+def dfs(G, starting_node, visited, checked):
+    if starting_node in visited:
+        return
+    if starting_node in checked:
+        return
+    if starting_node not in G:
+        return
+    visited.append(starting_node)
+
+    for n in G[starting_node]:
+        if n not in visited:
+            dfs(G, n, visited, checked)
+    return visited
 
 
 if __name__ == "__main__":
     # filename = input("Enter a filename: ")
-    filename = "LAB6-TEST-SET.txt"
+    filename = "example.txt"
     G = createGraph(filename)
     G_r = reverseGraph(G)
-    print(G)
-    # print(G_r)
-    # print("G_r")
-    # for node in G_r:
     visited = {}
-    mark(G, 1, visited, 1)
-    print(visited)
+    postOrder = [0]
+    mark(G, 1, visited, 1, postOrder)
+    post_order = createOrdering(visited, postOrder[0])
+    print(post_order)
+    checked = []
+    for post in range(len(post_order)-1, -1, -1):
+        node = post_order[post]
+        # if checked already, don't check
+        if node in checked:
+            continue
+        # marking everything connected to me
+        visited_r = []
+        dfs_arr = dfs(G_r, node, visited_r, checked)
+        if dfs_arr is not None:
+            checked += dfs_arr
+        print(node, visited_r)
